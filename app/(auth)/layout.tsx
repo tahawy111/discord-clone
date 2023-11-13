283dd4d3272696194e2dc0f10c56435e352cd628
tahawy111 <amertahawy111@gmail.com>
2023-11-14 01:09:23 +0200

    .

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,17 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 function AuthLayout({ children }: { children: React.ReactNode }) {
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/");
+    }
+  }, [status]);
   return <div className="h-full">{children}</div>;
 }
 
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,17 @@
 "use client";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 function MainLayout({ children }: { children: React.ReactNode }) {
   const { status } = useSession();
   const router = useRouter();
-  console.log(status);
 
-  if (status !== "authenticated") {
-    router.push("/login");
-  }
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    }
+  }, [status]);
+
   return <div className="h-full">{children}</div>;
 }
 

02d61d164ef881a21eb68f367b46b4186c60fbf9
tahawy111 <amertahawy111@gmail.com>
2024-01-29 20:44:33 +0200

    .

--- client/next.config.js
+++ client/next.config.js
@@ -11,6 +11,17 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
